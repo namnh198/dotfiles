83c678d45c9a5daff76dbb235269f5f5129e26ab
namnh198 <me@namhoainguyen.com>
2024-09-14 09:42:58 +0700

    chore: nvim: update plugins

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
+        "astro",
         "bash",
         "c",
         "diff",
@@ -29,8 +30,6 @@ return {
         "xml",
         "yaml",
       },
-      sync_install = false,
-      ignore_install = { "phpdoc", "comment" },
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = true,
@@ -52,31 +51,6 @@ return {
         enable_rename = true,
         enable_close = true,
         enable_close_on_slash = true, -- Important behavior in *.tsx
-      },
-      -- https://github.com/nvim-treesitter/playground#query-linter
-      query_linter = {
-        enable = true,
-        use_virtual_text = true,
-        lint_events = { "BufWrite", "CursorHold" },
-      },
-
-      playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = true, -- Whether the query persists across vim sessions
-        keybindings = {
-          toggle_query_editor = "o",
-          toggle_hl_groups = "i",
-          toggle_injected_languages = "t",
-          toggle_anonymous_nodes = "a",
-          toggle_language_display = "I",
-          focus_language = "f",
-          unfocus_language = "F",
-          update = "R",
-          goto_node = "<cr>",
-          show_help = "?",
-        },
       },
     },
   },
